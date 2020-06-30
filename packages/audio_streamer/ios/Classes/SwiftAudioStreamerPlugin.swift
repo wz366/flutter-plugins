7f6ddc45511a197c803d447384a55a1dbec696bb
Wes Zheng <wz366@users.noreply.github.com>
2020-06-30 15:51:31 -0700

    Update SwiftAudioStreamerPlugin.swift

--- packages/audio_streamer/ios/Classes/SwiftAudioStreamerPlugin.swift
+++ packages/audio_streamer/ios/Classes/SwiftAudioStreamerPlugin.swift
@@ -47,9 +47,12 @@ public class SwiftAudioStreamerPlugin: NSObject, FlutterPlugin, FlutterStreamHan
 
     func startRecording() {
         engine = AVAudioEngine()
+        
+        try! AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.record)
+      
         let input = engine.inputNode
         let bus = 0
-
+      
         input.installTap(onBus: bus, bufferSize: 22050, format: input.inputFormat(forBus: bus)) { (buffer, time) -> Void in
             let samples = buffer.floatChannelData?[0]
             // audio callback, samples in samples[0]...samples[buffer.frameLength-1]
